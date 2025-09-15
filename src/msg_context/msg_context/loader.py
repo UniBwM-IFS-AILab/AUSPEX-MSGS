a3be2f05c46fb478ad1cf844f7f0cc9c95dce76c
kaiosummer <ksf.sommer@web.de>
2025-09-15 16:29:44 +0200

    version 2.1

--- src/msg_context/msg_context/loader.py
+++ src/msg_context/msg_context/loader.py
@@ -3,7 +3,7 @@ import os
 
 if os.getenv('MSG_CONTEXT') == 'MENTHON':
     from hat_menthon.msg import Area, FrameData, PlatformState, PlatformCapabilities, PlatformClass, SearchMission, SensorCapabilities, SensorMode, UserCommand, Waypoints, BatteryState
-    from auspex_msgs.msg import ActionInstance, ActionsVLN, ExecuteAtom, KnowledgeChange, ObjectKnowledge, Plan, ActionStatus
+    from auspex_msgs.msg import ActionInstance, ActionsVLN, ExecuteAtom, KnowledgeChange, ObjectKnowledge, Plan, ActionStatus, ClientInfo
 else:
     from auspex_msgs.msg import Area, FrameData, PlatformState, PlatformCapabilities, PlatformClass, SearchMission, SensorCapabilities, SensorMode, UserCommand, Waypoints, BatteryState
-    from auspex_msgs.msg import ActionInstance, ActionsVLN, ExecuteAtom, KnowledgeChange, ObjectKnowledge, Plan, ActionStatus
+    from auspex_msgs.msg import ActionInstance, ActionsVLN, ExecuteAtom, KnowledgeChange, ObjectKnowledge, Plan, ActionStatus, ClientInfo

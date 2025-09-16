2fe5efbf4424cf278829a8b2c295e5faeaebde9f
kaiosummer <ksf.sommer@web.de>
2025-09-16 17:47:41 +0200

    version 2.2

--- src/msg_context_cpp/include/msg_context_cpp/message_loader.hpp
+++ src/msg_context_cpp/include/msg_context_cpp/message_loader.hpp
@@ -2,14 +2,12 @@
 #define MESSAGE_LOADER_HPP
 
 #ifdef HAVE_HAT_MENTHON  // Defined in CMakeLists.txt if hat_menthon is found AND MSG_CONTEXT=MENTHON
-    #include "hat_menthon/msg/user_command.hpp"
     #include "hat_menthon/msg/platform_state.hpp"
     #include "hat_menthon/msg/sensor_mode.hpp"
     #include "hat_menthon/msg/platform_capabilities.hpp"
     #include "hat_menthon/msg/frame_data.hpp"
     #include "hat_menthon/msg/battery_state.hpp"
 
-    using UserCommand = hat_menthon::msg::UserCommand;
     using SensorMode = hat_menthon::msg::SensorMode;
     using PlatformState = hat_menthon::msg::PlatformState;
     using PlatformCapabilities = hat_menthon::msg::PlatformCapabilities;
@@ -19,14 +17,12 @@
     using BatteryState = hat_menthon::msg::BatteryState;
 
 #else  // Default to auspex_msgs if hat_menthon is not found or MSG_CONTEXT != MENTHON
-    #include "auspex_msgs/msg/user_command.hpp"
     #include "auspex_msgs/msg/platform_state.hpp"
     #include "auspex_msgs/msg/sensor_mode.hpp"
     #include "auspex_msgs/msg/platform_capabilities.hpp"
     #include "auspex_msgs/msg/frame_data.hpp"
     #include "auspex_msgs/msg/battery_state.hpp"
 
-    using UserCommand = auspex_msgs::msg::UserCommand;
     using SensorMode = auspex_msgs::msg::SensorMode;
     using PlatformState = auspex_msgs::msg::PlatformState;
     using PlatformCapabilities = auspex_msgs::msg::PlatformCapabilities;

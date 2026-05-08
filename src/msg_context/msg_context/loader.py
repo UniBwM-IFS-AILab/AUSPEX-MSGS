086c7dfa5b011297d4b7e1b42134a64c91bec45e
doeschbj <bjoern-doeschl@gmx.de>
2026-05-08 15:56:39 +0200

    added heading

--- src/msg_context/msg_context/loader.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import os
-
-if os.getenv('MSG_CONTEXT') == 'MENTHON':
-    from hat_menthon.msg import Area, FrameData, PlatformState, PlatformCapabilities, PlatformClass, SearchMission, SensorCapabilities, SensorMode, UserCommand, Waypoints, BatteryState
-    from auspex_msgs.msg import ActionInstance, ActionsVLN, ExecuteAtom, KnowledgeChange, ObjectKnowledge, Plan, ActionStatus, ClientInfo
-else:
-    from auspex_msgs.msg import Area, FrameData, PlatformState, PlatformCapabilities, PlatformClass, SearchMission, SensorCapabilities, SensorMode, UserCommand, Waypoints, BatteryState
-    from auspex_msgs.msg import ActionInstance, ActionsVLN, ExecuteAtom, KnowledgeChange, ObjectKnowledge, Plan, ActionStatus, ClientInfo
